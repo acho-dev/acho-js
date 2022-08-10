d8ecaea735d4f4e99c093d056541cbbb4db68fd3
jianhao <jianhao@acho.io>
2022-08-10 14:31:12 -0400

    wip(test): resource endpoints

--- tests/resource.test.ts
+++ tests/resource.test.ts
@@ -6,35 +6,38 @@ describe('test resource endpoints', () => {
     endpoint: process.env.API_ENDPOINT ? process.env.API_ENDPOINT : 'http://localhost:8888'
   });
 
-  it('get simple resource table by assetId', async () => {
-    const data = await AchoInstance.ResourceEndpoints?.getTableData({ assetId: 9244 }); // Test get by assetId
-    expect(data).toBeInstanceOf(Object);
-    expect(data).toHaveProperty('data');
-    expect(data).toHaveProperty('schema');
-    expect(data).toHaveProperty('paging');
+  let assetResp: any;
+  let resResp: any;
 
-    const { data: _data, schema, paging } = data;
+  test('get simple resource table by assetId', async () => {
+    assetResp = await AchoInstance.ResourceEndpoints?.getTableData({ assetId: 9244 }); // Test get by assetId
+    expect(assetResp).toBeInstanceOf(Object);
+    expect(assetResp).toHaveProperty('data');
+    expect(assetResp).toHaveProperty('schema');
+    expect(assetResp).toHaveProperty('paging');
+
+    const { data: _data, schema, paging } = assetResp;
     expect(_data).toBeInstanceOf(Array);
-    expect(schema.toBeInstanceOf(Object));
-    expect(paging.toBeInstanceOf(Object));
+    expect(schema).toBeInstanceOf(Object);
+    expect(paging).toBeInstanceOf(Object);
   });
 
-  it('get simple resource table by resId', async () => {
-    const data = await AchoInstance.ResourceEndpoints?.getTableData({ resId: 4649 }); // Test get by resId
-    expect(data).toBeInstanceOf(Object);
-    expect(data).toHaveProperty('data');
-    expect(data).toHaveProperty('schema');
-    expect(data).toHaveProperty('paging');
+  test('get simple resource table by resId', async () => {
+    resResp = await AchoInstance.ResourceEndpoints?.getTableData({ resId: 4649 }); // Test get by resId
+    expect(resResp).toBeInstanceOf(Object);
+    expect(resResp).toHaveProperty('data');
+    expect(resResp).toHaveProperty('schema');
+    expect(resResp).toHaveProperty('paging');
 
-    const { data: _data, schema, paging } = data;
+    const { data: _data, schema, paging } = resResp;
     expect(_data).toBeInstanceOf(Array);
-    expect(schema.toBeInstanceOf(Object));
-    expect(paging.toBeInstanceOf(Object));
+    expect(schema).toBeInstanceOf(Object);
+    expect(paging).toBeInstanceOf(Object);
   });
 
-  it('resource table result consistency', async () => {
-    const assetResp = await AchoInstance.ResourceEndpoints?.getTableData({ assetId: 9244 }); // Test get by assetId
-    const resResp = await AchoInstance.ResourceEndpoints?.getTableData({ resId: 4649 }); // Test get by resId
+  test('resource table result consistency', async () => {
+    expect(assetResp).not.toBeUndefined();
+    expect(resResp).not.toBeUndefined();
     expect(assetResp).toEqual(resResp);
   });
 });
