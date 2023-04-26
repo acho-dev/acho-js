d44ddd9864ec476ed247084ec0b7115d39ce4258
jianhao <jianhao@acho.io>
2023-04-25 20:33:21 -0700

    feat(app): app socket room

---
+++ src/constants/index.ts
@@ -0,0 +1,2 @@
+export const SERVER_ADDRESS = process.env.VUE_APP_SERVER_ADDR || '';
+export const SOCKET_NAMESPACE = '/soc';
--- src/version.ts
+++ src/version.ts
@@ -1,12 +1,15 @@
 import { AchoClient } from '.';
 import { ClientOptions } from './types';
 import { App } from './app';
+import { SERVER_ADDRESS, SOCKET_NAMESPACE } from './constants';
+import { Socket, io } from 'socket.io-client';
 
 export class AppVersion {
   public appId: string;
   public verId: string;
   public metadata: any;
   public clientOpt: ClientOptions;
+  public room?: Socket;
   constructor(appId: string, verId: string, clientOpt?: ClientOptions) {
     this.appId = appId;
     this.verId = verId;
@@ -24,6 +27,13 @@ export class AppVersion {
       path: `/apps/${this.appId}/versions/${this.verId}`
     });
     this.metadata = verObj;
+    this.room = io(SERVER_ADDRESS + SOCKET_NAMESPACE, {
+      reconnectionAttempts: 10,
+      query: {
+        token: this.clientOpt.apiToken,
+        test: 'test parameter'
+      }
+    });
     return this.metadata;
   }
 }
