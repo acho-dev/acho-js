daae680b6c3e164a561e45b9888b3130b04edbf6
Gao Fan <gao@acho.io>
2023-07-21 10:34:28 +0800

    feat(proj): queryTableData

--- src/project.ts
+++ src/project.ts
@@ -1,4 +1,4 @@
-import { AchoClient, ProjectTableDataResp } from '.';
+import { AchoClient, ActionQuery, ProjectTableDataResp } from '.';
 import { ClientOptions } from './types';
 
 export interface getViewDataParams {
@@ -8,17 +8,46 @@ export interface getViewDataParams {
   pageSize?: number;
 }
 
+export interface queryTableDataParams {
+  actionQuery: ActionQuery;
+  page?: number;
+  pageSize?: number;
+  pageToken?: string;
+  jobId?: string;
+}
+
 export class ProjectEndpoints {
   private clientOpt: ClientOptions;
   constructor(clientOpt: ClientOptions) {
     this.clientOpt = clientOpt;
   }
+
   async getViewData(params: getViewDataParams) {
     const client: AchoClient = new AchoClient(this.clientOpt);
     const data: ProjectTableDataResp = await client.request({
       method: 'post',
       headers: {},
       path: '/project/get-view-data',
+      payload: params
+    });
+    return data;
+  }
+
+  /**
+   * Query resource table data
+   * @param {queryTableDataParams} params
+   * @param {ActionQuery} params.actionQuery - (required) the query and the helper info to query a resource table
+   * @param {number} params.page - the page of paged data
+   * @param {pageSize} params.pageSize - how many rows should be in one data page
+   * @param {string} params.jobId - required if you want to query other pages with the same actionQuery
+   * @returns {ProjectTableDataResp}
+   */
+  async queryTableData(params: queryTableDataParams) {
+    const client: AchoClient = new AchoClient(this.clientOpt);
+    const data: ProjectTableDataResp = await client.request({
+      method: 'post',
+      headers: {},
+      path: '/project/query',
       payload: params
     });
     return data;
